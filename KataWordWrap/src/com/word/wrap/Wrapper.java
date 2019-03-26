89e8f3ad1c8086fbb58c925a05fd13710cc4abdf
Aakanksha Mahajan <aakankm@bgsu.edu>
2019-03-25 21:07:32 -0400

    Changes to test case and documentation

--- KataWordWrap/src/com/word/wrap/Wrapper.java
+++ KataWordWrap/src/com/word/wrap/Wrapper.java
@@ -14,21 +14,18 @@ public class Wrapper {
 
 	/**
 	 * <code>wrap</code> is the only public static method in the class Wrapper which
-	 * correctly breaks the line.
+	 * correctly breaks the line such that every segment is less than the
+	 * columnNumber.
+	 * <li>If there is a space, then identify the last space, break the line before
+	 * the space and continue calling the same method with remaining line starting
+	 * from the character after the space</li>
 	 * <li>If there is a space at the column index, then append only the characters
-	 * till the index of the last space and continue calling the same method with
-	 * remaining line starting from the character after the space`
-	 * <li>
-	 * <li>If there is a space, then identify the last space, break the line till
-	 * before the space and continue calling the same method with remaining line
-	 * starting from the character after the space
-	 * <li>
+	 * till the index of the space and continue calling the same method with
+	 * remaining line starting from the character after the space`</li>
 	 * <li>If there is no space, then continue calling the same method with
-	 * remaining line starting from the index after the column number.
-	 * <li>
-	 * 
-	 * The function terminates when the input string's length is less than equal to
-	 * the column number
+	 * remaining line starting from the index after the column number.</li>The
+	 * function terminates when the input string's length is less than equal to the
+	 * column number
 	 * 
 	 * @param inputString
 	 * @param columnNumber
@@ -45,8 +42,7 @@ public class Wrapper {
 
 		int columnIndex = columnNumber;
 
-		// If the column number is not greater then equal to 1, then set the column
-		// number as 1
+		// If the column number is lesser than 1, then set the column number as 1
 		if (columnNumber < 1) {
 			columnIndex = 1;
 		}
@@ -64,6 +60,8 @@ public class Wrapper {
 			nextStringIndex = indexOfSpace + 1;
 		}
 
+		// add newline at the correct place and make recursive call with remaining
+		// string
 		return inputString.substring(0, subStringIndex) + NEW_LINE
 				+ wrap(inputString.substring(nextStringIndex), columnIndex);
 
--- KataWordWrap/test/com/word/wrap/WrapperTest.java
+++ KataWordWrap/test/com/word/wrap/WrapperTest.java
@@ -9,50 +9,49 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class WrapperTest {
 	@Test
-	public void testEmptyString() {
+	public void testEmptyInputString() {
 		assertEquals("", Wrapper.wrap("", 10));
 	}
 
 	@Test
-	public void testNullString() {
+	public void testNullInputString() {
 		assertEquals(null, Wrapper.wrap(null, 10));
 	}
 
 	@Test
-	public void testStringWithLengthLessThanColumn() {
+	public void testWithInputStringLengthLessThanColumn() {
 		assertEquals("Win It", Wrapper.wrap("Win It", 10));
 	}
 
 	@Test
-	public void testStringWhenColumnLessThan1() {
+	public void testWhenColumnNumberLessThan1() {
 		assertEquals("W\ni\nn\nI\nt", Wrapper.wrap("Win It", 0));
 	}
 
 	@Test
-	public void testInputWithOneWord() {
+	public void testWithOneWordInInputString() {
 		assertEquals("word\nword\nword\nword", Wrapper.wrap("wordwordwordword", 4));
 	}
 
 	@Test
-	public void testInputWithTwoWords() {
+	public void testWithTwoWordsInInputString() {
 		assertEquals("wordwor\ndword\nwordwor\ndword", Wrapper.wrap("wordwordword wordwordword", 7));
 	}
 
 	@Test
-	public void testInputWithNWords() {
-		assertEquals("wordwor\ndword\nwordwor\ndword\nwordwor\ndword",
-				Wrapper.wrap("wordwordword wordwordword wordwordword", 7));
+	public void testWithMultipleWordsInInputString() {
+		assertEquals("wordwor\ndword\nwordwor\ndword\nwordwor\ndword\nword\nword",
+				Wrapper.wrap("wordwordword wordwordword wordwordword word word", 7));
 	}
 
 	@Test
-	public void testInputWithSpecialCharacters() {
+	public void testWithSpecialCharactersInInputString() {
 		assertEquals("wordwor\ndword\nwordwor\n?dw!ord", Wrapper.wrap("wordwordword wordwor?dw!ord", 7));
 	}
-	
+
 	@Test
-	public void testInputWithSpaceAtColumnNumberIndex() {
+	public void testWithSpaceAtColumnNumberIndexInInputString() {
 		assertEquals("word\nword\nword\nword\nword\nword", Wrapper.wrap("wordword wordword wordword", 4));
 	}
-
 
 }
